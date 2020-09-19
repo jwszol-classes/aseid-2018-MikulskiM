4ee2b3488efd75d3e68732cf5a1d5674d2d4ed80
jokuniewska <julia.okuniewska1@gmail.com>
2020-09-19 18:47:33 +0200

    + calculate categories, +apply categories

--- examples/getting_and_printing_data.py
+++ examples/getting_and_printing_data.py
@@ -2,6 +2,8 @@ import warnings
 from podpac.datalib.terraintiles import TerrainTiles
 from podpac import Coordinates, clinspace
 import matplotlib.pyplot as plt
+import numpy as np
+from copy import copy, deepcopy
 
 # Let's just ignore warnings
 warnings.filterwarnings('ignore')
@@ -13,18 +15,49 @@ node = TerrainTiles(tile_format='geotiff', zoom=5)
 # Coordinates for both americas in one: Coordinates([clinspace(75, -60, 1000), clinspace(-155, -35, 1000)], dims=['lat', 'lon'])
 coords = Coordinates([clinspace(75, -60, 1000), clinspace(-155, -35, 1000)], dims=['lat', 'lon'])
 
+
+
+def split(array, nrows, ncols):
+    """Splits big matrix into array of submatrices (chunks)"""
+    r, h = array.shape
+    return array.reshape(h//nrows, nrows, -1, ncols).swapaxes(1, 2).reshape(-1, nrows, ncols)
+
+
+TOTAL_ITEMS = 1000 * 1000
+EDGE = int(np.sqrt(TOTAL_ITEMS))
+CHUNK_SIZE = 5
+
 # evaluate node
 ev = node.eval(coords)
-# node.download()
+
+data_np = np.asarray(ev.data)
+data_to_process = deepcopy(data_np)
+
+chunks = split(data_to_process, CHUNK_SIZE, CHUNK_SIZE)
+
+mean_chunks = np.asarray([np.max(x)-np.min(x) for x in chunks])
+mean_chunks = mean_chunks.reshape(int(EDGE/CHUNK_SIZE), int(EDGE/CHUNK_SIZE))
+
+result_lst = np.repeat(mean_chunks, CHUNK_SIZE, axis=0)
+result_lst = np.repeat(result_lst, CHUNK_SIZE, axis=1)
 
 
-print(ev)
+def ranges(start_val, stop_val, nb):
+    step = (stop_val - start_val) / nb
+    vals =  [(start_val + step*i) for i in range(nb)]
+    vals.append(stop_val)
+    return vals
 
-# plot the evaluation
-# fig = plt.figure()
-# ev.plot(vmin=0, cmap='terrain')
-#
-# show plot
-# plt.show()
 
-# input("waiting for enter or something")
+height_ranges = np.asarray(ranges(np.min(result_lst), np.max(result_lst), 6))
+# from ranges -> to categories 1...6
+result_lst = np.digitize(result_lst, bins=height_ranges, right=True)
+
+
+plt.subplot(211)
+plt.imshow(data_np)
+plt.subplot(212)
+plt.imshow(result_lst)
+plt.colorbar(ticks=range(0, 7))
+plt.legend()
+plt.show()
--- examples/random_values_processing_local_example.py
+++ examples/random_values_processing_local_example.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def split(array, nrows, ncols):
@@ -16,15 +17,35 @@ lst = np.random.randint(0, 10, TOTAL_ITEMS)\
 
 chunks = split(lst, CHUNK_SIZE, CHUNK_SIZE)
 
-mean_chunks = np.asarray([sum(sum(x))/CHUNK_SIZE**2 for x in chunks])
+mean_chunks = np.asarray([np.mean(x) for x in chunks])
 mean_chunks = mean_chunks.reshape(int(EDGE/CHUNK_SIZE), int(EDGE/CHUNK_SIZE))
 
 result_lst = np.repeat(mean_chunks, CHUNK_SIZE, axis=0)
 result_lst = np.repeat(result_lst, CHUNK_SIZE, axis=1)
 
 
+max_val = np.max(result_lst)
+min_val = np.min(result_lst)
+
+print(f"max value: {max_val}, min value: {min_val}")
+
+def ranges(start_val, stop_val, nb):
+    step = (stop_val - start_val) / nb
+    vals = [(start_val + step*i) for i in range(nb)]
+    vals.append(stop_val)
+    return vals
+
+
+height_ranges = np.asarray(ranges(np.min(result_lst), np.max(result_lst), 6))
+# from ranges -> to categories 1...6
+result_lst = np.digitize(result_lst, bins=height_ranges, right=True)
+
+
+print(result_lst)
 plt.subplot(211)
 plt.imshow(lst)
 plt.subplot(212)
 plt.imshow(result_lst)
+plt.colorbar(ticks=range(0, 7))
+plt.legend()
 plt.show()
